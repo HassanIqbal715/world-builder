4a2ba8700bfcdfd24a19a3854fb4701a43f08755
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2024-09-20 14:54:14 +0500

    Created functionality for Button Hover using enum for button's state. Created a class for the titletext.

--- title_screen.cpp
+++ title_screen.cpp
@@ -3,11 +3,40 @@
 
 using namespace std;
 
+enum BUTTON_STATE {IDLE, HOVER, PRESSED};
+
+class TitleText {
+private:
+	sf::Text text;
+	sf::Font font;
+
+public:
+	TitleText() {
+		font.loadFromFile("Fonts/font.ttf");
+		text.setFont(font);
+		text.setCharacterSize(100);
+		text.setFillColor(sf::Color::White);
+	}
+
+	void setText(string textString) {
+		text.setString(textString);
+	}
+
+	void setPosition(sf::Vector2f pos) {
+		text.setPosition(pos);
+	}
+
+	void render(sf::RenderWindow &window) {
+		window.draw(text);
+	}
+};
+
 class TitleButton {
 private:
 	sf::Text text;
 	sf::Font font;
 	sf::RectangleShape shape;
+	BUTTON_STATE BTN_STATE;
 
 public:
 	TitleButton() {
@@ -30,6 +59,31 @@ public:
 		text.setString(textString);
 		auto center = text.getLocalBounds().getSize() / 2.f;
 		text.setOrigin(center);
+	}
+
+	bool hover(sf::Vector2f mousePos) {
+		if (shape.getGlobalBounds().contains(mousePos))
+			return true;
+	}
+
+	void update(sf::Vector2f mousePos) {
+		BTN_STATE = IDLE;
+		if (hover(mousePos)) {
+			BTN_STATE = HOVER;
+		}
+
+		switch (BTN_STATE) {
+		case HOVER:
+			shape.setFillColor(sf::Color::Cyan);
+			break;
+
+		case IDLE:
+			shape.setFillColor(sf::Color::Black);
+			break;
+
+		default:
+			shape.setFillColor(sf::Color::Black);
+		}
 	}
 
 	void render(sf::RenderWindow &window) {
