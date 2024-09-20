0de9ee5474dd8a03aaa38432bba3103ee0157bc0
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2024-09-20 12:54:51 +0500

    Created some member functions for title_screen.cpp. Changed text's origin to liking. The button can be created now but it has no working.

--- title_screen.cpp
+++ title_screen.cpp
@@ -1,7 +1,7 @@
 #include <SFML\Graphics.hpp>
 #include <iostream>
-using namespace std;
 
+using namespace std;
 
 class TitleButton {
 private:
@@ -13,8 +13,28 @@ public:
 	TitleButton() {
 		font.loadFromFile("Fonts/font.ttf");
 		text.setFont(font);
+		text.setCharacterSize(40);
+		text.setFillColor(sf::Color::White);
 		shape.setSize(sf::Vector2f(300, 100));
 		shape.setOutlineThickness(2.0f);
+		shape.setFillColor(sf::Color::Black);
+		shape.setOutlineColor(sf::Color::White);
+	}
+
+	void setPosition(sf::Vector2f pos) {
+		shape.setPosition(pos);
+		text.setPosition(sf::Vector2f(pos.x + 150, pos.y + shape.getLocalBounds().getSize().y/3));
+	}
+
+	void setText(string textString) {
+		text.setString(textString);
+		auto center = text.getLocalBounds().getSize() / 2.f;
+		text.setOrigin(center);
+	}
+
+	void render(sf::RenderWindow &window) {
+		window.draw(shape);
+		window.draw(text);
 	}
 };
 
